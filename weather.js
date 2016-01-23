1a078f7604c370b94d671e4c11ee66cfccb46e8e
Kyle Paul <kpaul86+github@gmail.com>
2016-01-23 00:30:51 -0600

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,15 +1,15 @@
 (function() {
   var weather = function($http) {
     var getCityList = function(lat, lon) {
-      return $http.get('http://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon=' + lon + '&cnt=10&units=imperial');
+      return $http.get('https://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon=' + lon + '&cnt=10&units=imperial');
     };
 
     var getCity = function(searchCity) {
-      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&units=imperial');
+      return $http.get('https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&units=imperial');
     };
 
     var getCityForecast = function(cityId) {
-      return $http.get('http://api.openweathermap.org/data/2.5/forecast?id=' + cityId + '&units=imperial');
+      return $http.get('https://api.openweathermap.org/data/2.5/forecast?id=' + cityId + '&units=imperial');
     };
 
     return {
